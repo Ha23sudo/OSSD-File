1471444b2be078c5e84fbc6577ad323ac9812384
hassaan <hassaan.ahmad@umt.edu.pk>
2026-04-30 14:55:59 +0500

    Section y7 Gui code

--- Week9_y7.py
+++ Week9_y7.py
@@ -1,11 +1,31 @@
 
 import tkinter as  tk
 
+def add():
+    val1=float(n1.get())
+    val2=float(n2.get())
+    res=val1+val2
+    print(res)
+    result.config(text="Result: "+str(res))
+
 root=tk.Tk()
 root.title("Calculator")
 root.geometry("300x250")
-tk.Button(root,text="Sign Up").pack(pady=10)
+root.resizable(False,False)
+root.config(bg="lightblue")
+root.iconphoto(False, tk.PhotoImage(file="im.png"))
 
+tk.Label(root, text="Enter Value 1").pack()
+n1=tk.Entry(root)
+n1.pack()
+tk.Label(root, text="Enter Value 2").pack()
+n2=tk.Entry(root)
+n2.pack()
+tk.Button(root, text="Add",command=add).pack()
+tk.Button(root, text="-").pack()
+
+result=tk.Label(root, text="Result:")
+result.pack()
 
 root.mainloop()
 
---
+++ cal_y7.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("300x400")
+
+tk.Label(root, "Enter Value 1").pack()
+
+
+root.mainloop()
+
+
+
